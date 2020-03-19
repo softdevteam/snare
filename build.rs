1755c7980545ba13a47f725d1227a9e5d5085938
Laurence Tratt <laurie@tratt.net>
2020-03-19 17:50:00 +0000

    Don't recompile everything if non-code files change.

--- build.rs
+++ build.rs
@@ -1,8 +1,10 @@
 use cfgrammar::yacc::YaccKind;
 use lrlex::LexerBuilder;
 use lrpar::CTParserBuilder;
+use rerun_except::rerun_except;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    rerun_except(&["snare.1", "snare.conf.5", "snare.conf.example"])?;
     let lex_rule_ids_map = CTParserBuilder::<u8>::new_with_storaget()
         .yacckind(YaccKind::Grmtools)
         .process_file_in_src("config.y")?;
